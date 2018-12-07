cafd9a83403ac003d0b38540289c7368cfc86460
Kalle Ott <kalle.ott@cap3.de>
2018-12-07 21:22:46 +0100

    show toast on cache update

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,2 +1,47 @@
 // @ts-check
+import { toast, ToastContainer } from 'react-toastify'
+import React from 'react'
+import { css } from 'emotion'
+import { PageReloadToast } from './src/components/PageReloadToast'
 import 'prismjs/themes/prism-tomorrow.css'
+import 'react-toastify/dist/ReactToastify.css'
+import { colors } from './src/styles/variables'
+
+const darkToast = css({
+  background: colors.dark,
+  color: colors.light,
+  '.Toastify__close-button--default': {
+    color: colors.light,
+  },
+})
+
+const fixMobileWidth = css({
+  '@media only screen and (max-width: 480px)': {
+    width: '100%',
+  },
+})
+
+const showReloadToast = () => {
+  const dismiss = () => {
+    toast.dismiss(id)
+  }
+
+  const id = toast(<PageReloadToast dismiss={dismiss} />, {
+    autoClose: false,
+    position: 'bottom-right',
+    className: darkToast,
+  })
+}
+
+export const onServiceWorkerUpdateFound = () => {
+  showReloadToast()
+}
+
+export const wrapRootElement = ({ element }) => {
+  return (
+    <React.Fragment>
+      {element}
+      <ToastContainer className={fixMobileWidth} />
+    </React.Fragment>
+  )
+}
