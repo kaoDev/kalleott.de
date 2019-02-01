3a794400ddfe015b9835cbc957e83c40c9236415
Kalle Ott <kalle.ott@cap3.de>
2019-02-01 20:56:19 +0100

    generate sitemap

--- gatsby-config.js
+++ gatsby-config.js
@@ -138,6 +138,7 @@ module.exports = {
     'gatsby-plugin-sharp',
     'gatsby-transformer-sharp',
     'gatsby-plugin-react-helmet',
+    `gatsby-plugin-sitemap`,
     {
       resolve: `gatsby-plugin-feed`,
       options: {
