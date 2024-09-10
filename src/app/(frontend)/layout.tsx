2a6f3ec1348c97e50b006d777176186a9138a355
Kalle Ott <mail@kalleott.de>
2024-09-10 11:25:53 +0200

    restore analytics

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -1,27 +1,33 @@
-import { cn } from '@/utilities/cn'
-import { GeistMono } from 'geist/font/mono'
-import { GeistSans } from 'geist/font/sans'
-import type { Metadata } from 'next'
-import React from 'react'
-import { AdminBar } from '../components/AdminBar'
-import { Footer } from '../components/Footer'
-import { Header } from '../components/Header'
-import { LivePreviewListener } from '../components/LivePreviewListener'
-import { Providers } from '../providers'
-import { InitTheme } from '../providers/Theme/InitTheme'
-import './globals.css'
+import { cn } from "@/utilities/cn";
+import { GeistMono } from "geist/font/mono";
+import { GeistSans } from "geist/font/sans";
+import type { Metadata } from "next";
+import React from "react";
+import { AdminBar } from "../components/AdminBar";
+import { Footer } from "../components/Footer";
+import { Header } from "../components/Header";
+import { LivePreviewListener } from "../components/LivePreviewListener";
+import { Providers } from "../providers";
+import { InitTheme } from "../providers/Theme/InitTheme";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import "./globals.css";
 
 export default async function RootLayout({
   children,
   modal,
 }: {
-  children: React.ReactNode
-  modal: React.ReactNode
+  children: React.ReactNode;
+  modal: React.ReactNode;
 }) {
   return (
-    <html className={cn(GeistSans.variable, GeistMono.variable)} lang="en" suppressHydrationWarning>
+    <html
+      className={cn(GeistSans.variable, GeistMono.variable)}
+      lang="en"
+      suppressHydrationWarning
+    >
       <head>
-        {['light', 'dark'].map((theme) => (
+        {["light", "dark"].map((theme) => (
           <>
             <link
               rel="icon"
@@ -50,13 +56,15 @@ export default async function RootLayout({
           <div className="flex-1" />
           <Footer />
         </Providers>
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
-  )
+  );
 }
 
 export const metadata: Metadata = {
-  title: 'Kalle Ott',
+  title: "Kalle Ott",
   description:
     "Hi I'm Kalle, a software engineer working in the Berlin startup scene. This is my blog where I write about software engineering, programming, and other things that interest me.",
-}
+};
